6e7d5892fd64aad15f0517bf60160770c1ca14c8
arincdemir <68223368+arincdemir@users.noreply.github.com>
2023-05-23 14:36:54 +0300

    more cleanup in colorbutton

--- colorbutton.cpp
+++ colorbutton.cpp
@@ -9,19 +9,6 @@ ColorButton::ColorButton(MyGrid *grid, int identifier,
     this->identifier = identifier;
     this->grid = grid;
     open = false;
-    this->color = color;
-    QPalette pal = palette();
-    if(color == "blue"){
-        pal.setColor(QPalette::Button, QColor(222,222,222));
-    }else if(color == "red"){
-        pal.setColor(QPalette::Button, QColor(Qt::red));
-    }else{
-        pal.setColor(QPalette::Button, QColor(Qt::green));
-    }
-    setFlat(true);
-    setAutoFillBackground(true);
-    setPalette(pal);
-    update();
 }
 
 void ColorButton::pressed() {
